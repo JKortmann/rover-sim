bf1dc706f7958e3fa30b0cf874f8d33206b04fe7
Marcel Pfeifer <marcel@wolves.ink>
2021-01-11 22:52:03 +0100

    feat: :bug: Fix turning

--- src/util/turnVehicle.ts
+++ src/util/turnVehicle.ts
@@ -1,5 +1,5 @@
 export const turnVehicle = (turnDegree: number): number[] => {
-	let speed = Math.abs(turnDegree) > 20 ? 0.6 : 0.2;
+	let speed = Math.abs(turnDegree) > 20 ? 0.8 : 0.585;
 	if (turnDegree < 0) {
 		return [-speed, speed];
 	} else {
