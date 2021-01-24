d1fd24eed13ee743a75e20b63fdb193601d2f549
Marcel Pfeifer <marcel@wolves.ink>
2021-01-24 17:58:50 +0100

    refactor: :recycle: Improvements

--- src/util/functions/avoidObstacles.ts
+++ src/util/functions/avoidObstacles.ts
@@ -1,10 +1,18 @@
 import { Engines } from 'rover';
 
-export const avoidObstacles = (engines: Engines, proximityArray: number[], desiredHeadingDelta: number) => {
+export const avoidObstacles = (
+	engines: Engines,
+	proximityArray: number[],
+	desiredHeadingDelta: number,
+	minDistanceToObstacle = 4
+) => {
 	const closestPointProximity = Math.min(...proximityArray);
 	const closestPointAngle = (360 / proximityArray.length) * proximityArray.indexOf(Math.min(...proximityArray));
 
-	if (closestPointProximity < 4 && !(Math.abs(closestPointAngle - desiredHeadingDelta) >= 180)) {
+	const isCloseToObstacle = closestPointProximity < minDistanceToObstacle;
+	const passedObstacle = !(Math.abs(closestPointAngle - desiredHeadingDelta) >= 180);
+
+	if (isCloseToObstacle && passedObstacle) {
 		engines = [0.6, 0.6, 0.6, 0.6, 0.6, 0.6];
 		engines = engines.map((e, i) => {
 			if ((closestPointAngle / 90) % 2 > 1) {
